bdd7bb9e36eb3b2fcbc12d554ef5ef7c358c29de
Enes Eren <thenesern@gmail.com>
2022-08-26 22:21:35 +0300

    Bugfix on the Schema exporting

--- backend/src/utils/getGoogleUser.js
+++ backend/src/utils/getGoogleUser.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const getAccessAndBearerTokenUrl = ({ accessToken }) =>
   `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${accessToken}`;
 
-module.exports = getGoogleUser = async ({ code }) => {
+module.exports.getGoogleUser = async ({ code }) => {
   const { tokens } = await oAuthGoogleClient.getToken(code);
   const response = await axios.get(
     getAccessAndBearerTokenUrl({ accessToken: tokens.access_token }),
